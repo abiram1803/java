deaf0a94862ead6bfc0671c05000401ef4925f5e
abiram1803 <rshreeabiram@gmail.com>
2026-08-30 20:05:09 +0530

    Initial commit with Java snippet

---
+++ src/Snippet.java
@@ -0,0 +1,8 @@
+public class Snippet {
+    public static void main(String[] args) {
+        printMessage();
+    }
+    public static void printMessage() {
+        System.out.println("Hello from the new Java repository webhook!");
+    }
+}
